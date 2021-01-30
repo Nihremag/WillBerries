41b3abb540237eed4cc469726d4e3dd85becc061
Radmir <radmir_96@mail.ru>
2021-01-30 16:12:08 +0500

    Добавлена плавная прокрутка и hover эффекты

--- js/main.js
+++ js/main.js
@@ -20,3 +20,12 @@ const swiper = new Swiper('.swiper-container', {
       el: '.swiper-scrollbar',
     },
   });
+
+  function ScrollUp(){
+    let t,s;
+    s = document.body.scrollTop||window.pageYOffset;
+    t = setInterval(function()
+    {
+      if(s > 0)window.scroll(0, s-=5);
+      else clearInterval(t)},0.5);
+    }
